b6a9f054ddeb211aed4681214a23a9a20414208d
mlmillanp <mlmillanp@gmail.com>
2023-01-16 15:02:11 +0100

    Colocado comentario en el main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,9 @@
 package org.example;
 
 public class Main {
+    /*
+        Funcion principal
+     */
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
